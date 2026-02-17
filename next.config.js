aebfa9b74e0bda16e9f31e781ba61f968580418f
Doghouser11 <doghouser11@gmail.com>
2026-02-17 16:20:16 +0200

    Fix: correct serverExternalPackages for Next.js

--- next.config.js
+++ next.config.js
@@ -1,24 +1,43 @@
+
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['prisma']
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'localhost',
-      },
-      {
-        protocol: 'https', 
-        hostname: 'momentia.online',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-      }
-    ],
-  },
+
+serverExternalPackages: ['prisma'],
+
+images: {
+
+remotePatterns: [
+
+{
+
+protocol: 'https',
+
+hostname: 'localhost',
+
+},
+
+{
+
+protocol: 'https',
+
+hostname: 'momentia.online',
+
+},
+
+{
+
+protocol: 'http',
+
+hostname: 'localhost',
+
+}
+
+],
+
+},
+
 }
 
 module.exports = nextConfig
+
